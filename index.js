e5a822438955232b10b7c857f8780c77e9e1a499
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-05-06 19:40:21 +0530

    add catch method

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.post('/webhook', line.middleware(config), (req, res) => {
    Promise
        .all(req.body.events.map(handleEvent))
        .then((result) => res.json(result))
-       
+       .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      });
 });
 
 // event handler
