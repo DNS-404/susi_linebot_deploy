60e8d81b1332c001aff1c556a483c3e17f1ae658
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-05-06 19:37:57 +0530

    test5

--- index.js
+++ index.js
@@ -26,10 +26,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
    Promise
        .all(req.body.events.map(handleEvent))
        .then((result) => res.json(result))
-       .catch((err) => {
-        console.error(err);
-        res.status(500).end();
-      });
+       
 });
 
 // event handler
